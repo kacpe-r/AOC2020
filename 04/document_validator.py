85c70a12be1845d1ba790e41cc903dc9f3edc689
Kacper <chrzanowski.kacper@gmail.com>
2020-12-04 22:13:33 +0100

    Day #4 part 2

--- 04/document_validator.py
+++ 04/document_validator.py
@@ -1,6 +1,8 @@
-def get_number_of_valid_documents(file_path):
+from re import search
+
+def get_valid_documents(file_path):
+    valid_users = []
     mandatory_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    number_of_valid_documents = 0
     with open(file_path) as f:
         persons = f.read().split('\n\n')
         for person in persons:
@@ -8,8 +10,31 @@ def get_number_of_valid_documents(file_path):
             person_dict = dict(kv.split(':') for kv in person_formatted)
             is_document_valid = all(elem in person_dict.keys() for elem in mandatory_fields)
             if is_document_valid:
-                number_of_valid_documents += 1
-    return number_of_valid_documents
+                valid_users.append(person_dict)
+    return valid_users
+
+def is_height_correct(height):
+    if 'cm' in height:
+        return 149 < int(''.join(filter(str.isdigit, height))) < 194
+    else:
+        return 58 < int(''.join(filter(str.isdigit, height))) < 77
+
+def get_number_of_strong_validated_documents(prevalidated_documents):
+    valid_documents = 0
+    for document in prevalidated_documents:
+        validators = [
+            int(document['byr']) in range(1920, 2003),
+            int(document['iyr']) in range(2010, 2021),
+            int(document['eyr']) in range(2020, 2031),
+            is_height_correct(document['hgt']),
+            search(r'^#[0-9a-fA-F]{6}$', document['hcl']),
+            document['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+            search(r'^\d{9}$', document['pid']),
+        ]
+
+        if all(validators): valid_documents += 1
+    return valid_documents
 
 if __name__ == '__main__':
-    print(get_number_of_valid_documents('./data'))
+    print(len(get_valid_documents('./data')))
+    print(get_number_of_strong_validated_documents(get_valid_documents('./data')))
