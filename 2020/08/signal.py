94088badd074a2b737283c10d41d76cbec0de917
Kacper <chrzanowski.kacper@gmail.com>
2020-12-08 23:14:03 +0100

    2020 #08 - remove unused variable

--- 2020/08/signal.py
+++ 2020/08/signal.py
@@ -6,7 +6,7 @@ def get_data(file_path):
         return [(lambda x: [x[0], int(x[1])])(single_row.split(' ')) for single_row in data]
 
 
-def get_accumulator_value(data, data_index, visited_indexes, acc_sum, iterator_index = 0):
+def get_accumulator_value(data, data_index, visited_indexes, acc_sum):
     message_type, move = data[data_index]
     if data_index not in visited_indexes:
         visited_indexes.append(data_index)
@@ -31,14 +31,14 @@ def get_changable_elements():
             changable_elements_indexes.append(index)
     return changable_elements_indexes
 
-def jmp_to_nop(data, iteration = None):
+def jmp_to_nop(data):
     indexes_of_changable_elements = get_changable_elements()
     for found_index in indexes_of_changable_elements:
         new_data = deepcopy(data)
         new_data[found_index][0] = 'nop'
 
         try:
-            get_accumulator_value(new_data, 0, [], 0, iteration)
+            get_accumulator_value(new_data, 0, [], 0)
         except IndexError:
             break
         except Exception:
