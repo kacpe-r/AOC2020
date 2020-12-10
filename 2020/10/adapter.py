c15a35ea77bffa83ca15f5a6d541a4cfa4cf3c93
Kacper <chrzanowski.kacper@gmail.com>
2020-12-10 22:33:21 +0100

    2020 #10 part 2/2

--- 2020/10/adapter.py
+++ 2020/10/adapter.py
@@ -1,4 +1,4 @@
-import itertools
+from collections import defaultdict
 
 def get_data(file_path):
     with open(file_path) as f:
@@ -30,6 +30,22 @@ def get_adapters(full_list):
     used_three = sorted(list(set(used_three)))
     return len(used_one) * len(used_three)
 
+def get_all_combinations(data_list):
+    data_list = sorted(data_list)
+    data_list.append(data_list[-1] + 3)
+
+    new_dictionary = defaultdict(int)
+    new_dictionary[0] = 1
+
+    for i in range(0, len(data_list)):
+        for j in reversed(range(i)):
+            if (data_list[i] - data_list[j]) > 3:
+                break
+            new_dictionary[i] += new_dictionary[j]
+
+    return new_dictionary[len(data_list) - 1]
+
 if __name__ == '__main__':
     full_list = sorted(get_data('./data'))
     print(get_adapters(full_list))
+    print(get_all_combinations(full_list))
